0005dc697a584a5407d117190b71086c7aca74a7
rZk1809 <rohithgankan@gmail.com>
2025-06-06 19:53:57 +0530

    added navbar

--- UserFrontend/src/App.jsx
+++ UserFrontend/src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Home from './pages/Home/Home'
+import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart'
 import {assets} from './assets/assets'
-
+import LoginPopup from './components/LoginPopup/LoginPopup'
+import Checkout from './pages/Checkout/Checkout' 
+import MyCourses from './pages/MyCourses/MyCourses';
 const App = () => {
 
+  const [showLogin,setShowLogin] = useState(false);
+
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin}/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/checkout' element={<Checkout/>}/> 
+          <Route path='/my-courses' element={<MyCourses/>}/>
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
--- UserFrontend/src/components/Navbar/Navbar.jsx
+++ UserFrontend/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,31 @@
 import React, { useContext, useState } from 'react'
-import  './Navbar.css'
+import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
 import { StoreContext } from '../../Context/StoreContext'
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
 
-  const [menu,setMenu] = useState("home");
-  const {getTotalCartAmount} = useContext(StoreContext);
+    const [menu, setMenu] = useState("home");
+    const { getTotalCartAmount } = useContext(StoreContext);
 
-  return (
-    <div className='navbar'>
-      <Link to='/'><img className='logo' src={assets.logo} alt="" /></Link>
-      <ul className="navbar-menu">
-        <Link to="/" onClick={()=>setMenu("home")} className={`${menu==="home"?"active":""}`}>Home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={`${menu==="menu"?"active":""}`}>Course</a>
-        <a href='#footer' onClick={()=>setMenu("contact")} className={`${menu==="contact"?"active":""}`}>Contact us</a>
-      </ul>
-      <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
-        <Link to='/cart' className='navbar-search-icon'>
-          <img src={assets.basket_icon} alt="" />
-          <div className={getTotalCartAmount()>0?"dot":""}></div>
-        </Link>
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
-      </div>
-    </div>
-  )
+    return (
+        <div className='navbar'>
+            <Link to='/'><img className='logo' src={assets.logo} alt="Learn-It Logo" /></Link>
+            <ul className="navbar-menu">
+                <Link to="/" onClick={() => setMenu("home")} className={`${menu === "home" ? "active" : ""}`}>Home</Link>
+                <a href='#explore-categories' onClick={() => setMenu("categories")} className={`${menu === "categories" ? "active" : ""}`}>Categories</a>
+                <a href='#footer' onClick={() => setMenu("contact")} className={`${menu === "contact" ? "active" : ""}`}>Contact us</a>
+            </ul>
+            <div className="navbar-right">
+                <Link to='/cart' className='navbar-search-icon'>
+                    <img src={assets.cart_icon} alt="Cart" />
+                    <div className={getTotalCartAmount() > 0 ? "dot" : ""}></div>
+                </Link>
+                <button onClick={() => setShowLogin(true)}>Sign In</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
