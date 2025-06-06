c692c899a6bcf7c454f4a1168e04a68296e6cee4
rZk1809 <rohithgankan@gmail.com>
2025-06-06 20:25:38 +0530

    Added Header and Loginpopup

---
+++ UserFrontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Unlock Your Potential, One Course at a Time</h2>
+                <p>Explore thousands of expert-led online courses on Learn-It. Master job-ready skills in technology, business, and the arts. Start your learning journey today!</p>
+                <a href="#explore-categories"><button>View Courses</button></a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
