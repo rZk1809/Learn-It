7f483db751e1fb5fc2e0a6a8e85abcbef4037f5b
rZk1809 <rohithgankan@gmail.com>
2025-06-06 08:50:48 +0530

    testing

--- UserFrontend/src/App.jsx
+++ UserFrontend/src/App.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <ul>
+        <li>
+          <a href="/api/hello">Hello API</a>
+        </li>
+        <li>
+          <a href="/api/hello-world">Hello World API</a>
+        </li>
+        <li>
+          <a href="/api/hello-world-2">Hello World 2 API</a>
+        </li>
+      </ul>
     </div>
   )
 }
